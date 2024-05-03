b254e86c553aa25a463a2eb2c2a3da89510a95d5
Gus G <gus.gran@gmail.com>
2024-05-03 13:53:35 -0700

    Update FluxSim to the new format

--- projects.py
+++ projects.py
@@ -43,8 +43,8 @@ simulations = {
                    "February 2021", "...", ["Simulation", "Software"]),
     "simulator": Project("Simulator", "simulation/simulator", f"{image_prefix}/Simulator.png",
                     "April 2015", "May 2024", ["Simulation", "Software"]),
-    "flux": Project("Flux Sim", "SimulationProjects/FluxSim", f"{image_prefix}/FluxSim.png",
-                    "August 2014",  "...", ["Simulation", "Software"]),
+    "fluxsim": Project("Flux Sim", "simulation/fluxsim", f"{image_prefix}/FluxSim.png",
+                    "August 2014",  "May 2024", ["Simulation", "Software"]),
     "beam": Project("Beam Flow", "SimulationProjects/BeamFlow", f"{image_prefix}/BeamFlow.png",
                     "December 2011",  "...", ["Simulation", "Software"]),
     "vec": Project("Vector Flow", "SimulationProjects/VectorFlow", f"{image_prefix}/VectorFlow.png",
@@ -60,7 +60,7 @@ def simulation():
 @projects.route("/simulation/<project>")
 def simulation_project(project: str):
     lower_project = project.lower()
-    if lower_project in simulations:
+    if lower_project in simulations and GITHUB_PREFIX not in simulations[lower_project].link:
         return render_template(f"/simulation/{escape(project)}.html", project=simulations[lower_project])
     else:
         return render_template("errors/not_found.html")
