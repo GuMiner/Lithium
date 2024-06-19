b34451fd1223922e7f41c0c450f308906f4b36d5
Gus G <gus.gran@gmail.com>
2024-06-19 11:42:55 -0700

    Work on the puzzle area, filling in more H24 functions

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import datetime
 from werkzeug import exceptions
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, g
 from flask_socketio import SocketIO, emit
 from flask_compress import Compress
-from pages import projects, puzzles, games, recommendations
-
-# TODO -- move most of this into separate files.
-import sqlite3
+from pages import base
 
 app = Flask(__name__)
+base.APP = app  # Allows for blueprints to access and use the app instance.
+
+# Must be imported later so that 'base.APP' is not None
+from pages import games, projects, puzzles, recommendations
+
 app.config['SECRET_KEY'] = {'UNUSED'}
 socketio = SocketIO(app)
 
 Compress(app)
-app.register_blueprint(projects.projects)
 app.register_blueprint(games.games)
+app.register_blueprint(projects.projects)
+app.register_blueprint(puzzles.puzzles)
 app.register_blueprint(recommendations.recommendations)
 
 # Used by all pages for the ©️ text.
@@ -25,18 +28,6 @@ def inject_year():
 @app.errorhandler(exceptions.NotFound)
 def handle_not_found(error):
     return render_template("errors/not_found.html")
-
-def get_puzzle_db():
-    db = getattr(g, '_puzzle_data_db', None)
-    if db is None:
-        db = g._database = sqlite3.connect("db/puzzle-data.db")
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_puzzle_data_db', None)
-    if db is not None:
-        db.close()
 
 @app.route("/")
 def index():
@@ -49,29 +40,6 @@ def attributions():
 @app.route("/diagnostics")
 def diagnostics():
     return render_template("diagnostics.html")
-
-@app.route("/puzzles")
-def puzzles_page():
-    return render_template("puzzles.html")
-
-@app.route("/puzzles/words", methods=['POST'])
-def puzzles_word_query():
-    query = request.form.get("wordQuery")
-    anagrams = request.form.get("anagrams") is not None
-
-    prefix = '<textarea id="resultingWords" readonly>'
-
-    limit = 200
-    if anagrams:
-        words = [word[0] for word in get_puzzle_db()
-                 .execute(puzzles.get_anagram_query(query), [limit])]
-    else:
-        words = [word[0] for word in get_puzzle_db()
-                .execute(puzzles.get_word_query(), [query, limit])]
-
-    limitText = ' (Limited!)' if len(words) == limit else ''
-    postfix = f'</textarea><small>{len(words)}{limitText}</small>'
-    return prefix + '\n'.join(words) + postfix
 
 @socketio.on('block-sync')
 def sync(message):
--- pages/puzzles.py
+++ pages/puzzles.py
@@ -1,7 +1,39 @@
-def get_word_query():
+from dataclasses import dataclass
+from flask import Blueprint, render_template, request, g
+import sqlite3
+from typing import List
+
+from . import base
+
+puzzles = Blueprint('puzzles', __name__, url_prefix='/puzzles', template_folder='../templates')
+
+def _get_puzzle_db():
+    db = getattr(g, '_puzzle_data_db', None)
+    if db is None:
+        db = g._database = sqlite3.connect("db/puzzle-data.db")
+    return db
+
+@base.APP.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_puzzle_data_db', None)
+    if db is not None:
+        db.close()
+
+def _get_word_query():
     return "SELECT word from words WHERE word LIKE ? ORDER BY word LIMIT ?"
 
-def get_anagram_query(query):
+def _get_thesaurus_query():
+    return """SELECT T.word, L.synonymList
+FROM thesaurus T
+JOIN thesaurus_lookup L
+ON SUBSTR(T.synonymIds, 0, INSTR(T.synonymIds, ',')) = L.id
+WHERE word LIKE ? ORDER BY word LIMIT ?
+"""
+
+def _get_homophones_query():
+    return "SELECT homophones FROM homophones WHERE UPPER(homophones) LIKE ? ORDER BY homophones LIMIT ?"
+
+def _get_anagram_query(query):
     lookup = {}
     for character in query:
         if not character in lookup:
@@ -17,3 +49,105 @@ def get_anagram_query(query):
             sql_query += f" AND (LENGTH(word) - LENGTH(REPLACE(word, '{key}', '')) {comparison} {value})"
 
     return sql_query + ' LIMIT ?'
+
+@dataclass
+class Link:
+    name: str
+    link: str
+
+@dataclass
+class Reference:
+    title: str
+    links: List[Link]
+
+references = [
+    [
+        Reference("Art", [
+            Link("Lists of Celebrities", "https://en.wikipedia.org/wiki/Lists_of_celebrities")
+            ]),
+        Reference("Financial", [
+            Link("NYSE Stock Listings", "https://www.nyse.com/listings_directory/stock")
+            ]),
+    ],
+    [
+        Reference("Geography", [
+           Link("Countries", "https://en.wikipedia.org/wiki/List_of_sovereign_states"),
+           Link("US States and Capitals", "https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States"),
+           Link("Country Calling Codes", "https://en.wikipedia.org/wiki/List_of_country_calling_codes")
+        ]),
+        Reference("Science", [
+            Link("Chemical Elements", "https://en.wikipedia.org/wiki/List_of_chemical_elements"),
+            Link("Resistor Color Codes", "https://en.wikipedia.org/wiki/Electronic_color_code"),
+        ]),
+    ],
+    [
+        Reference("Codes", [
+              Link("American Sign Language","https://upload.wikimedia.org/wikipedia/commons/d/d1/Asl_alphabet_gallaudet.png"), 
+              Link("Braille", "https://www.pharmabraille.com/pharmaceutical-braille/the-braille-alphabet/"), 
+              Link("Maritime Signal Flags", "https://en.wikipedia.org/wiki/International_maritime_signal_flags"),
+              Link("NATO Phonetic Alphabet", "https://upload.wikimedia.org/wikipedia/commons/e/e0/FAA_Phonetic_and_Morse_Chart2.svg"),
+              Link("Pigpen", "https://upload.wikimedia.org/wikipedia/commons/3/36/Pigpen_cipher_key.svg"),
+              Link("Semaphore Flags", "https://en.wikipedia.org/wiki/Flag_semaphore"),
+        ])
+    ],
+    [   
+        Reference("Sports", [
+
+            Link("Sports teams in US and Canada", "https://en.wikipedia.org/wiki/List_of_professional_sports_teams_in_the_United_States_and_Canada"),
+            Link("Football teams in US and Canada", "https://en.wikipedia.org/wiki/National_Football_League"),
+            Link("Soccer clubs in England", "https://en.wikipedia.org/wiki/List_of_football_clubs_in_England"),
+        ])
+    ]
+]
+
+@puzzles.route("/")
+def puzzles_page():
+    return render_template("puzzles.html", references=references)
+
+def _generate_textarea(id, items, limit):
+    prefix = f'<textarea id="{id}" readonly>'
+    limitText = ' (Limited!)' if len(items) == limit else ''
+    postfix = f'</textarea><small>{len(items)}{limitText}</small>'
+    return prefix + '\n'.join(items) + postfix
+
+@puzzles.route("/crossword", methods=['POST'])
+def puzzles_crossword_querty():
+    query = request.form.get("crosswordQuery").upper()
+
+    limit = 200
+    questions = [word[0] for word in _get_puzzle_db()
+            .execute(_get_crossword_question_query(), [query, limit])]
+    answers = [word[0] for word in _get_puzzle_db()
+            .execute(_get_crossword_answer_query(), [query, limit])]
+
+    return _generate_textarea("foundCrossword", words, limit)
+
+@puzzles.route("/wordsExtra", methods=['POST'])
+def puzzles_extra_query():
+    query = request.form.get('wordExtraQuery').upper()
+    extraChoice = request.form.get('extraChoice')
+
+    limit = 200
+    if extraChoice == 'Thesaurus':
+        words = [f'{word[0]} -> {word[1]}' for word in _get_puzzle_db()
+                 .execute(_get_thesaurus_query(), [query, limit])]
+    else:
+        words = [word[0] for word in _get_puzzle_db()
+                .execute(_get_homophones_query(), [f'{query}%', limit])]
+        
+    return _generate_textarea("resultingExtraWords", words, limit)
+
+@puzzles.route("/words", methods=['POST'])
+def puzzles_word_query():
+    query = request.form.get("wordQuery").upper()
+    anagrams = request.form.get("anagrams") is not None
+
+    limit = 200
+    if anagrams:
+        words = [word[0] for word in _get_puzzle_db()
+                 .execute(_get_anagram_query(query), [limit])]
+    else:
+        words = [word[0] for word in _get_puzzle_db()
+                .execute(_get_word_query(), [query, limit])]
+
+    return _generate_textarea("resultingWords", words, limit)
