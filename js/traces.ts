5b361f4a4b55b748960c3c91a37d0843bc16b4b1
Gus G <gus.gran@gmail.com>
2024-09-15 17:46:36 -0700

    Get continual redraws working

--- js/traces.ts
+++ js/traces.ts
@@ -2,6 +2,15 @@ var canvasDiv = null;
 
 // The best WebGPU source I've found is https://math.hws.edu/graphicsbook/
 
+let device: GPUDevice;
+let context: GPUCanvasContext;
+let pipeline: GPURenderPipeline;
+let vertexBuffer: GPUBuffer;
+let uniformBuffer: GPUBuffer;
+let uniformBindGroup: GPUBindGroup;
+
+let shader: GPUShaderModule;
+
 function renderErrorMessage(canvas, ...errorMessages: String[]) {
     const context = canvas.getContext("2d");
 
@@ -29,9 +38,9 @@ async function initWebGPU(canvas): Promise<boolean> {
         return false;
     }
 
-    let device = await adapter.requestDevice();
+    device = await adapter.requestDevice();
 
-    let context = canvas.getContext("webgpu");
+    context = canvas.getContext("webgpu");
     context.configure({
         device: device,
         format: navigator.gpu.getPreferredCanvasFormat(),
@@ -54,7 +63,7 @@ async function initWebGPU(canvas): Promise<boolean> {
         `;
 
     device.pushErrorScope("validation");
-    let shader = device.createShaderModule({ code: shaderSource });
+    shader = device.createShaderModule({ code: shaderSource });
 
     let error = await device.popErrorScope();
     if (error) {
@@ -95,14 +104,14 @@ async function initWebGPU(canvas): Promise<boolean> {
         layout: gpuPipelineLayout
     };
 
-    let pipeline = device.createRenderPipeline(pipelineDescriptor);
+    pipeline = device.createRenderPipeline(pipelineDescriptor);
 
-    let uniformBuffer = device.createBuffer({
+    uniformBuffer = device.createBuffer({
         size: 3 * 4,
         usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
     });
 
-    let uniformBindGroup = device.createBindGroup({
+    uniformBindGroup = device.createBindGroup({
         layout: uniformBindGroupLayout,
         entries: [
             {
@@ -116,36 +125,14 @@ async function initWebGPU(canvas): Promise<boolean> {
         -0.8, -0.6, 0.8, -0.6, 0, 0.7
     ]); // X-1=Left, X+1=Right, Y-1=Bottom, Y+1=Top
 
-    let vertexBuffer = device.createBuffer({
+    vertexBuffer = device.createBuffer({
         size: vertexCoords.byteLength,
         usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
     });
 
     device.queue.writeBuffer(vertexBuffer, 0, vertexCoords);
-    device.queue.writeBuffer(uniformBuffer, 0, new Float32Array([1.0, 0.5, 0.8]));
 
-
-    let commandEncoder = device.createCommandEncoder();
-
-    let colorAttachment: GPURenderPassColorAttachment = {
-        clearValue: { r: 0.5, g: 0.5, b: 0.5, a: 1 },  // gray background
-        loadOp: "clear", // Alternative is "load".
-        storeOp: "store",  // Alternative is "discard".
-        view: context.getCurrentTexture().createView()  // Draw to the canvas.
-    };
-
-    let renderPassDescriptor: GPURenderPassDescriptor = { colorAttachments: [colorAttachment] };
-
-    let passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-    passEncoder.setPipeline(pipeline);            // Specify pipeline.
-    passEncoder.setVertexBuffer(0, vertexBuffer);  // Attach vertex buffer.
-    passEncoder.setBindGroup(0, uniformBindGroup); // Attach bind group.
-    passEncoder.draw(3);                          // Generate vertices.
-    passEncoder.end();
-
-
-    let commandBuffer = commandEncoder.finish();
-    device.queue.submit([commandBuffer]);
+    return true;
 }
 
 
@@ -158,6 +145,9 @@ function enterFullscreen() {
 }
 
 (window as any).enterFullscreen = enterFullscreen;
+
+let previousTime = null;
+
 window.addEventListener('load', async () => {
     // Setup the screen
     canvasDiv = document.getElementById("canvasDiv");
@@ -175,9 +165,42 @@ window.addEventListener('load', async () => {
     console.log(`${cssWidth}x${cssHeight}`)
 
     if (await initWebGPU(canvas)) {
-        // Continue
+        previousTime = performance.now();
+        requestAnimationFrame(renderFrame);
     }
 });
+
+function renderFrame() {
+    let now = performance.now();
+    let timeDelta = (now - previousTime) / 1000; 
+    previousTime = now;
+
+    device.queue.writeBuffer(uniformBuffer, 0, new Float32Array([Math.random(), 0.5, 0.8]));
+
+
+    let colorAttachment: GPURenderPassColorAttachment = {
+        clearValue: { r: 0.5, g: 0.5, b: 0.5, a: 1 },  // gray background
+        loadOp: "clear", // Alternative is "load".
+        storeOp: "store",  // Alternative is "discard".
+        view: context.getCurrentTexture().createView()  // Draw to the canvas.
+    };
+
+    let commandEncoder = device.createCommandEncoder();
+
+    let renderPassDescriptor: GPURenderPassDescriptor = { colorAttachments: [colorAttachment] };
+
+    let passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.setPipeline(pipeline);            // Specify pipeline.
+    passEncoder.setVertexBuffer(0, vertexBuffer);  // Attach vertex buffer.
+    passEncoder.setBindGroup(0, uniformBindGroup); // Attach bind group.
+    passEncoder.draw(3);                          // Generate vertices.
+    passEncoder.end();
+
+    let commandBuffer = commandEncoder.finish();
+    device.queue.submit([commandBuffer]);
+
+    requestAnimationFrame(renderFrame);
+}
 
 window.addEventListener("resize", () => {
     let canvas = document.getElementById("tracesCanvas");
