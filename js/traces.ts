9ef1874a146c6e1f9eaa444dd62f9b83e39d4aef
Gus G <gus.gran@gmail.com>
2024-10-27 10:42:12 -0700

    Get a basic full screen buffer render working:

--- js/traces.ts
+++ js/traces.ts
@@ -1,14 +1,15 @@
 import { GPUHelper } from "./traces/utility";
 
-var canvasDiv = null;
-
-// The best WebGPU source I've found is https://math.hws.edu/graphicsbook/
+var canvasDiv: HTMLDivElement = null;
+var canvas: HTMLCanvasElement = null;
 
 interface Program {
     vertexBuffer: GPUBuffer;
     vertexCount: number;
     uniformBuffer: GPUBuffer;
-    uniformBindGroup: GPUBindGroup;
+
+    // Must be ordered in the order they are used in the shader.
+    bindGroups: GPUBindGroup[];
     pipeline: GPURenderPipeline;
 }
 
@@ -17,18 +18,36 @@ class GridProgram implements Program {
     
     vertexBuffer: GPUBuffer;
     vertexCount: number;
+
     uniformBuffer: GPUBuffer;
     uniformBindGroup: GPUBindGroup;
+
+    gridBuffer: GPUBuffer;
+    gridBufferBindGroup: GPUBindGroup;
+
+    bindGroups: GPUBindGroup[];
+
     pipeline: GPURenderPipeline;
+
+    readonly simWidth = 320;
+    readonly simHeight = 240;
 
     // This structure is because TypeScript does not allow async constructors.
     protected constructor(shader: GPUShaderModule) {
         this.shader = shader;
 
-        // TODO -- these belong in a subclass, somehow.
+        // Viewport size
         this.uniformBuffer = device.createBuffer({
-            size: 3 * 4,
+            label: "ViewportBuffer",
+            size: 2 * 4,
             usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+        });
+
+        // grid data
+        this.gridBuffer = device.createBuffer({
+            label: "GridBuffer",
+            size: 4*this.simWidth*this.simHeight,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
         });
 
         let uniformFragmentLayout: GPUBindGroupLayoutEntry = {
@@ -40,16 +59,41 @@ class GridProgram implements Program {
 
         // vec3f(4-byte) color
         this.uniformBindGroup = device.createBindGroup({
+            label: "UniformBindGroup",
             layout: uniformBindGroupLayout,
             entries: [{
                 binding: 0,
-                resource: { buffer: this.uniformBuffer, offset: 0, size: 3 * 4 }
+                resource: { buffer: this.uniformBuffer, offset: 0, size: 2 * 4 }
                 }
             ]
         });
 
+        let storageFragmentLayout: GPUBindGroupLayoutEntry = {
+            binding: 0,
+            visibility: GPUShaderStage.COMPUTE | GPUShaderStage.FRAGMENT,
+            buffer: { type: "storage" }
+        };
+        let gridBufferBindGroupLayout = device.createBindGroupLayout({ entries: [storageFragmentLayout] });
+
+        // vec3f(4-byte) color
+        this.gridBufferBindGroup = device.createBindGroup({
+            label: "GridBufferBindGroup",
+            layout: gridBufferBindGroupLayout,
+            entries: [{
+                binding: 0,
+                resource: { buffer: this.gridBuffer, offset: 0, size: 4*this.simWidth*this.simHeight }
+            }
+            ]
+        });
+
+        this.bindGroups = [this.gridBufferBindGroup, this.uniformBindGroup];
+
+        this.populateGridBuffer(device);
+
+        // Cor
         const vertexCoords = new Float32Array([   // 2D coords for drawing a triangle.
-            -0.8, -0.6, 0.8, -0.6, 0, 0.7
+            -1,-1, 1,-1, 1,1,
+            -1,-1, 1,1, -1,1
         ]); // X-1=Left, X+1=Right, Y-1=Bottom, Y+1=Top
     
         this.vertexBuffer = device.createBuffer({
@@ -58,7 +102,7 @@ class GridProgram implements Program {
         });
     
         device.queue.writeBuffer(this.vertexBuffer, 0, vertexCoords);
-        this.vertexCount = 3;
+        this.vertexCount = vertexCoords.length / 2;
         
         let vertexBufferLayout: GPUVertexBufferLayout[] = [ // An array of vertex buffer specifications.
         {
@@ -68,7 +112,8 @@ class GridProgram implements Program {
         }];
     
         
-        let gpuPipelineLayout = device.createPipelineLayout({ bindGroupLayouts: [uniformBindGroupLayout] });
+        let gpuPipelineLayout = device.createPipelineLayout({
+            bindGroupLayouts: [gridBufferBindGroupLayout, uniformBindGroupLayout] });
         let pipelineDescriptor: GPURenderPipelineDescriptor = {
             vertex: { // Configuration for the vertex shader.
                 module: this.shader,
@@ -91,6 +136,23 @@ class GridProgram implements Program {
         this.pipeline = device.createRenderPipeline(pipelineDescriptor);
     }
 
+    populateGridBuffer(device: GPUDevice) {
+        // Put the starting values into the buffer
+        let colorData = new Uint32Array(this.simWidth*this.simHeight);
+        let counter = 0;
+
+        // Temporarily fill with a repeating pattern
+        for (let i = 0; i < this.simWidth * this.simHeight; i++) {
+            colorData[i] = counter;
+            counter++;
+            if (counter > 5) {
+                counter = 0;
+            }
+        }
+
+        device.queue.writeBuffer(this.gridBuffer, 0, colorData);
+    }
+
     static async new(shaderUri: string): Promise<GridProgram> {
         return new GridProgram(await GPUHelper.loadShader(device, shaderUri));
     }
@@ -103,6 +165,7 @@ class TriangleProgram implements Program {
     vertexCount: number;
     uniformBuffer: GPUBuffer;
     uniformBindGroup: GPUBindGroup;
+    bindGroups: GPUBindGroup[];
     pipeline: GPURenderPipeline;
 
     // This structure is because TypeScript does not allow async constructors.
@@ -131,6 +194,8 @@ class TriangleProgram implements Program {
                 }
             ]
         });
+
+        this.bindGroups = [this.uniformBindGroup];
 
         const vertexCoords = new Float32Array([   // 2D coords for drawing a triangle.
             -0.8, -0.6, 0.8, -0.6, 0, 0.7
@@ -229,8 +294,8 @@ let previousTime = null;
 
 window.addEventListener('load', async () => {
     // Setup the screen
-    canvasDiv = document.getElementById("canvasDiv");
-    var canvas = document.createElement("canvas");
+    canvasDiv = document.getElementById("canvasDiv") as HTMLDivElement;
+    canvas = document.createElement("canvas");
     canvas.style.width = "100%";
     canvas.style.height = "100%";
     canvas.id = "tracesCanvas";
@@ -264,12 +329,16 @@ function renderFrame() {
         device.queue.writeBuffer(program.uniformBuffer, 0, new Float32Array([Math.random(), 0.5, 0.8]));
     } else if (mode == 1) {
         program = gridProgram;
+
+        device.queue.writeBuffer(program.uniformBuffer, 0, new Float32Array([canvas.clientWidth, canvas.clientHeight]));
     }
 
     // Just like OpenGL, attach everything listed for drawing.
     passEncoder.setPipeline(program.pipeline);
     passEncoder.setVertexBuffer(0, program.vertexBuffer);
-    passEncoder.setBindGroup(0, program.uniformBindGroup);
+    for (let i = 0; i < program.bindGroups.length; i++) {
+        passEncoder.setBindGroup(i, program.bindGroups[i]);
+    }
     passEncoder.draw(program.vertexCount);
     passEncoder.end();
 
--- static/game/gpu/grid.wgsl
+++ static/game/gpu/grid.wgsl
@@ -1,11 +1,32 @@
-@group(0) @binding(0) var<uniform> color : vec3f;
+// White-to-black color scale
+const colors = array<vec4f, 6>(
+    vec4f(1.0, 1.0, 1.0, 1.0),
+    vec4f(0.8, 0.8, 0.8, 1.0),
+    vec4f(0.6, 0.6, 0.6, 1.0),
+    vec4f(0.4, 0.4, 0.4, 1.0),
+    vec4f(0.2, 0.2, 0.2, 1.0),
+    vec4f(0.0, 0.0, 0.0, 1.0));
+
+const simWidth = 320u;
+const simHeight = 240u;
+
+// Index into colors for the simulation
+@group(0) @binding(0) var<storage,read_write> gridData : array<u32, simWidth*simHeight>;
+@group(1) @binding(0) var<uniform> viewport: vec2f;
 
 @vertex
-fn vertexMain( @location(0) coords : vec2f ) -> @builtin(position) vec4f {
-    return vec4f( coords, 0, 1 );
+fn vertexMain( @location(0) coords: vec2f ) -> @builtin(position) vec4f {
+    return vec4f(coords, 0, 1);
 }
 
 @fragment
-fn fragmentMain() -> @location(0) vec4f {
-    return vec4f( color, 1 ); 
+fn fragmentMain( @builtin(position) position: vec4f) -> @location(0) vec4f {
+    // position will be the 0 to viewport pixel coordinate
+    // We need to map the sim width and height to that entire view area
+    let gridDataIndex = vec2u((position.xy / viewport) * vec2f(f32(simWidth), f32(simHeight)));
+
+    // Then grab the correct color matching to the current simulation index
+    let dataIndex = u32(gridDataIndex.y)*simWidth + u32(gridDataIndex.x);
+    let colorIndex = gridData[dataIndex];
+    return colors[colorIndex];
 }
